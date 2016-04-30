adac10a1f6c216016c260aaa84060be716385260
akilawickey <asak999@gmail.com>
2016-05-01 00:46:01 +0530

    for my appu

---
+++ facebookrestapi/src/main/java/com/vlearn/facebookrestapi/Interaction.java
@@ -0,0 +1,48 @@
+package com.vlearn.facebookrestapi;
+
+public class Interaction {
+	
+	private Profile profile;
+	private String keyword;
+	
+	public Interaction (Profile profile,String keyword){
+		
+		this.profile = profile;
+		this.keyword = keyword;
+		
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	public String interact(Profile profile,String keyword){
+		
+//		switch (keyword){
+//		
+//			case 
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+	}
+	
+
+}
